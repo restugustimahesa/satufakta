89ae15a9510abac877d268aa9dde675affcee366
diar-kun <diaricakarsa05@gmail.com>
2025-05-27 21:36:47 +0700

    menambahkan login & register

---
+++ lib/views/resgister_screen.dart
@@ -0,0 +1,316 @@
+// lib/login_page.dart
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:satufakta/views/login_screen.dart';
+import 'package:satufakta/views/home_screen.dart';
+import 'package:satufakta/views/utils/helper.dart';
+
+class ResgisterScreen extends StatefulWidget {
+  const ResgisterScreen({super.key});
+
+  @override
+  State<ResgisterScreen> createState() => _ResgisterScreenState();
+}
+
+class _ResgisterScreenState extends State<ResgisterScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _termsAccepted = false;
+
+  // Perkiraan Warna dari Gambar
+  static const Color primaryButtonColor = Color(0xFFF76788);
+  static const Color linkTextColor = Color(0xFFFFA726);
+  static const Color checkboxActiveColor = linkTextColor;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: cWhite,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Buat Akun',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: cBlack,
+                  ),
+                ),
+                Text(
+                  'Mari bantu Anda menyiapkan akun,',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: cBlack,
+                  ),
+                ),
+                Text(
+                  'proses ini tidak akan memakan waktu lama.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: cBlack,
+                  ),
+                ),
+                
+                const SizedBox(height: 48),
+
+                // nama
+                Text(
+                  'Nama',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: cBlack,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Masukkan Nama',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: primaryButtonColor, width: 1.5),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                  ),
+                ),
+                vsLarge,
+
+                // Email
+                Text(
+                  'Email',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: cBlack,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Masukkan Email',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: primaryButtonColor, width: 1.5),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                  ),
+                ),
+                vsLarge,
+
+                // Password
+                const Text(
+                  'Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Masukkan Password',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: primaryButtonColor, width: 1.5),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                  ),
+                ),
+                vsLarge,
+
+                // Konfirmasi Password
+                const Text(
+                  'Konfirmasi Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Tulis ulang Password',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: primaryButtonColor, width: 1.5),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                  ),
+                ),
+                vsLarge,
+
+                // Terima Syarat dan Ketentuan
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center, // Menyelaraskan secara vertikal
+                  children: <Widget>[
+                  SizedBox(
+                    height: 24.0, // Untuk mengatur area tap checkbox
+                    width: 24.0,  // Untuk mengatur area tap checkbox
+                    child: Checkbox(
+                    value: _termsAccepted,
+                    onChanged: (bool? value) {
+                      setState(() {
+                      _termsAccepted = value ?? false;
+                      });
+                    },
+                    activeColor: checkboxActiveColor,
+                    side: BorderSide(color: linkTextColor), // Warna border saat tidak aktif
+                    visualDensity: VisualDensity.compact, // Membuat checkbox sedikit lebih kecil
+                    ),
+                  ),
+                  const SizedBox(width: 8), // Jarak antara checkbox dan teks
+                  GestureDetector(
+                    onTap: () {
+                    setState(() {
+                      _termsAccepted = !_termsAccepted;
+                    });
+                    },
+                    child: const Text(
+                    'Terima syarat & ketentuan',
+                    style: TextStyle(
+                      color: linkTextColor,
+                      fontSize: 14,
+                    ),
+                    ),
+                  ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+
+                // Tombol Sign In
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryButtonColor,
+                    minimumSize: const Size(double.infinity, 52),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    elevation: 2,
+                  ),
+                  onPressed: () {
+                    // if (_formKey.currentState!.validate()) { // Aktifkan jika ingin validasi
+                      // Proses login
+                      Navigator.pushReplacement( // Ganti halaman agar tidak bisa kembali ke login
+                        context,
+                        MaterialPageRoute(builder: (context) => const HomeScreen()),
+                      );
+                    // }
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Icon(Icons.arrow_forward_outlined, color: Colors.white, size: 20),
+                    ],
+                  ),
+                ),
+                vsMassive,
+
+                // Belum memiliki akun? Daftar
+                Align(
+                  alignment: Alignment.center,
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Sudah memiliki akun? ',
+                      style: const TextStyle(color: Colors.black87, fontSize: 15),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Masuk',
+                          style: const TextStyle(
+                            color: linkTextColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            decoration: TextDecoration.underline,
+                            decorationColor: linkTextColor,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LoginScreen()),
+                              );
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
